52d20be969bf049a873c280b9d0031a40a7772c9
silserena <sserena292@gmail.com>
2024-07-06 19:10:47 +0200

    Slider

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let appearElement = document.querySelectorAll('.appear')
 window.addEventListener('scroll', () =>{
     appearElement.forEach( (appear) =>{
         if (appear.getBoundingClientRect().top < window.innerHeight - 150 ) {
-            appear.style.opacity = '1'  /** appear.style.maxWidth = '1'*/
+            appear.style.opacity = '1'  
            
         } else {
             appear.removeAttribute('style')
@@ -11,3 +11,29 @@ window.addEventListener('scroll', () =>{
     } )
 
 })
+
+// slider
+let imagenes = document.querySelectorAll('.slider__img') 
+let slider = document.querySelector('.slider') 
+let index = 0 
+
+
+
+// - Funciones
+
+setInterval (() => {
+    if (index < imagenes.length - 1) {
+        index++ 
+        let calculate = index *-100
+        slider.style.transform = `translateX(${calculate}vw)`
+    } else if (index == imagenes.length-1) {
+            index = 0//
+            let calculate = index *-100
+            slider.style.transform = `translateX(${calculate}vw)`
+         
+        }
+
+
+}, 5000);
+
+

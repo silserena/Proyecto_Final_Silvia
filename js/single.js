496f1ff6993092099f046a674fe8db810afbb02a
silserena <sserena292@gmail.com>
2024-07-13 12:59:54 +0200

    single creacion

---
+++ js/single.js
@@ -0,0 +1,30 @@
+/**PARALLAX */
+
+let title = document.querySelector('h1')
+let video = document.querySelector('video')
+
+window.addEventListener('scroll', () =>{
+
+    
+    let scroll = window.pageYOffset
+    let speed = 0.3
+
+    title.style.transform = 'translateY('+scroll * speed+'px)'
+
+})
+
+/**APPEAR MW*/
+
+let appearElement = document.querySelectorAll('.appear')
+
+window.addEventListener('scroll', () =>{
+    appearElement.forEach( (appear) =>{
+        if (appear.getBoundingClientRect().top < window.innerHeight - 150 ) {
+            appear.style.opacity = '1'  
+           
+        } else {
+            appear.removeAttribute('style')
+        }
+    } )
+
+})

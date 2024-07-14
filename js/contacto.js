11a609cbbf982e44211946ef262ecc3800027818
silserena <sserena292@gmail.com>
2024-07-15 01:33:22 +0200

    crear contacto

---
+++ js/contacto.js
@@ -0,0 +1,25 @@
+let appearElement = document.querySelectorAll('.appear')
+
+window.addEventListener('scroll', () =>{
+    appearElement.forEach( (appear) =>{
+        if (appear.getBoundingClientRect().top < window.innerHeight - 150 ) {
+            appear.style.opacity = '1'  
+           
+        } else {
+            appear.removeAttribute('style')
+        }
+    } )
+
+})
+/** menu hamburguesa */
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})

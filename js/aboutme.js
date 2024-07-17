834ed1704fb89cc68681f7cab410d15ed4474d77
silserena <sserena292@gmail.com>
2024-07-17 20:23:50 +0200

    comentarios del codigo

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,7 +1,12 @@
-/** appear */
-let appearElement = document.querySelectorAll('.appear') 
+/**________________________________
+ *  Appear - me gustaba la idea de que fuese apareciendo todo poco a poco, segun vas haciendo scroll en la web. Me parece que queda muy bonito
+ * 
+*/
+let appearElement = document.querySelectorAll('.appear') // declaracion de la variable
 
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () =>{ // función arrow
+
+   
     appearElement.forEach( (appear) =>{
         if (appear.getBoundingClientRect().top < window.innerHeight - 150 ) {
             appear.style.opacity = '1'  
@@ -12,7 +17,7 @@ window.addEventListener('scroll', () =>{
     } )
 
 })
-/** menu hamburguesa */
+/**------- menu hamburguesa-------------- */
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
